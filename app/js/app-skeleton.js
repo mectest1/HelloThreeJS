e2f4886fec0b084c22d0808ead14415d4d8eb9bc
mec_test_1 <mec_test_1@hotmail.com>
2017-02-21 12:50:10 +0800

    prepare skeletons for the following three.js apps;

---
+++ app/js/app-skeleton.js
@@ -0,0 +1,9 @@
+define(['THREE', 'angular', 'jquery'], 
+	function(THREE, angular, jquery){
+	
+	return angular.module('threeApp', [])
+			.controller('defaultCtrl', ['$scope', function($scope){
+					
+	}]);
+	
+});
