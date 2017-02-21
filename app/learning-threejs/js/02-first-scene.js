528470e318bb0b7e801927b988597f32023cad6e
mec_test_1 <mec_test_1@hotmail.com>
2017-02-21 13:29:16 +0800

    Prepare the first three.js scene;

---
+++ app/learning-threejs/js/02-first-scene.js
@@ -0,0 +1,82 @@
+define(['THREE', 'angular', 'jquery'], 
+	function(THREE, angular, jquery){
+	
+	return angular.module('threeApp', [])
+			.constant('config', {
+				canvasId: 'main-canvas'
+			})
+			.controller('defaultCtrl', ['$scope', 'config', function($scope, config){
+		const {Scene, PerspectiveCamera, WebGLRenderer, AxisHelper,
+				PlaneGeometry, MeshBasicMaterial, Mesh,
+				BoxGeometry, SphereGeometry} = THREE;	
+		const CANVAS_WIDTH = window.innerWidth;
+		const CANVAS_HEIGHT = window.innerHeight;
+		const {canvasId} = config;
+		const canvasEle = jquery('#' + canvasId);
+		
+		function init(){
+			let scene = new Scene();
+			let camera = new PerspectiveCamera(45, CANVAS_WIDTH/CANVAS_HEIGHT, 
+									0.1, 1000);
+			
+			let renderer = new WebGLRenderer();
+			renderer.setClearColor(0xEEEEEE);
+			renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+			
+			let axes = new AxisHelper(20);
+			scene.add(axes);
+			
+			let planeGeometry = new PlaneGeometry(60, 20, 1, 1);
+			let planeMaterial = new MeshBasicMaterial({
+				color: 0xCCCCCC
+			});
+			let plane = new Mesh(planeGeometry, planeMaterial);
+			
+			plane.rotation = {
+				x: -0.5 * Math.PI
+			};
+			plane.position = {
+				x: 15, y: 0, z: 0
+			};
+			scene.add(plane);
+			
+			let cubeGeometry = new BoxGeometry(4, 4, 4);
+			let cubeMaterial = new MeshBasicMaterial({
+				color: 0xFF0000,
+				wireframe: true
+			});
+			let cube = new Mesh(cubeGeometry, cubeMaterial);
+			
+			cube.position = {
+				x: -4, y: 3, z: 0
+			};
+			scene.add(cube);
+			
+			let sphereGeometry = new SphereGeometry(4, 20, 20);
+			let sphereMaterial = new MeshBasicMaterial({
+				color: 0x7777FF,
+				wireframe: true
+			});
+			let sphere = new Mesh(sphereGeometry, sphereMaterial);
+			
+			sphere.position = {
+				x: 20,
+				y: 4,
+				z: 2
+			};
+			scene.add(sphere);
+			
+			camera.position = {
+				x: -30, y: 40, z: 30
+			};
+			camera.lookAt(scene.position);
+			
+		
+			canvasEle.append(renderer.domElement);
+			renderer.render(scene, camera);
+		}
+		
+		init();
+	}]);
+	
+});
