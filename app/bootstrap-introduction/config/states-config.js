1409f861ca681b96abb71c5c6b92dad17ce632bd
mec_test_1 <mec_test_1@hotmail.com>
2017-04-03 21:21:09 +0800

    play with more bootstrap

--- app/bootstrap-introduction/config/states-config.js
+++ app/bootstrap-introduction/config/states-config.js
@@ -6,16 +6,20 @@ define(['angular', 'THREE', 'jquery',
 	'text!../views/01-basic-grid-system-view.html',
 	'text!../views/02-basic-typogrpahy.html',
 	'text!../views/03-basic-lists.html',
-	'text!../views/04-basic-table-style.html'
-	
-
+	'text!../views/04-basic-table-style.html',
+	'text!../views/05-basic-form-style.html',
+	'text!../views/06-basic-form-style-pt2.html',
+	'text!../views/07-basic-form-style-pt3.html'
 ], function(angular, THREE, jquery,
 	homeStateController,
 	homeStateView,
 	_01BasicGridSystemView,
 	_02BasicTypographyView,
 	_03BasiclistsView,
-	_04BasicTableStyleView
+	_04BasicTableStyleView,
+	_05BasicFormStyleView,
+	_06BasicFormPt2View,
+	_07BasicFormPt3View
 ){
 	'use strict';
 	const homeState = {
@@ -49,6 +53,22 @@ define(['angular', 'THREE', 'jquery',
 			parent: homeState,
 			url: '04-basic-table',
 			template: _04BasicTableStyleView
+		},
+		{
+			name: '05-basic-form',
+			parent: homeState,
+			url: '/05-basic-form',
+			template: _05BasicFormStyleView
+		},{
+			name: '06-basic-form-pt2',
+			parent: homeState,
+			url: '/06-basic-form-pt2',
+			template: _06BasicFormPt2View
+		},{
+			name: '07-basic-form-pt3',
+			parent: homeState,
+			url: '/07-basic-form-pt3',
+			template: _07BasicFormPt3View
 		}
 	];
 	return states;
